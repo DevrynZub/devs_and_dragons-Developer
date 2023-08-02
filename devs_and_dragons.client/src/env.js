8d4e60ec578f3956f20fe7e50ff1f83ad9378428
Andrew Allison <andrewallison5211@gmail.com>
2023-08-02 16:05:41 -0600

    project spun up

--- devs_and_dragons.client/src/env.js
+++ devs_and_dragons.client/src/env.js
@@ -1,6 +1,9 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-74troqf32atk1sf4.us.auth0.com'
+export const clientId = '6y8FgqSRtZwVSJvNDZe6GvBpGGekZsTM'
+export const audience = 'classroom.com'
+
+
+
