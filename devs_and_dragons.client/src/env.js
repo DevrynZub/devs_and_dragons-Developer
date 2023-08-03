f788da9c8bbc524cb5a04aab5af10fcbb13a5bd0
Devryn <devryn.zub@gmail.com>
2023-08-03 12:24:34 -0700

    merging

--- devs_and_dragons.client/src/env.js
+++ devs_and_dragons.client/src/env.js
@@ -2,10 +2,8 @@ export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
 export const domain = 'dev-74troqf32atk1sf4.us.auth0.com'
-export const clientId = 'classroom.com'
-export const audience = '6y8FgqSRtZwVSJvNDZe6GvBpGGekZsTM'
+export const clientId = '6y8FgqSRtZwVSJvNDZe6GvBpGGekZsTM'
+export const audience = 'classroom.com'
 
 
-AUTH_DOMAIN = dev - 74troqf32atk1sf4.us.auth0.com
-AUTH_AUDIENCE = classroom.com
-AUTH_CLIENT_ID = 6y8FgqSRtZwVSJvNDZe6GvBpGGekZsTM
+
