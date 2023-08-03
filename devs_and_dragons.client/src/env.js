a168309851f1d4e343ff32cbf0e31b890971761d
Devryn <devryn.zub@gmail.com>
2023-08-03 12:15:59 -0700

    updates for me

--- devs_and_dragons.client/src/env.js
+++ devs_and_dragons.client/src/env.js
@@ -1,6 +1,9 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-zvdpzy77bw1f4rhb.us.auth0.com'
+export const clientId = 'ufRJMtKSXsPtZHX8jL0LM9neKd8iNnJd'
+export const audience = 'classroom.com'
+
+
+
