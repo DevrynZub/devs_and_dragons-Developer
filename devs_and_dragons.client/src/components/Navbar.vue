97c195f4e02aa3ef342cce12d2b1684c5cead3ff
Riley <riley@idahosuperiorcustoms.com>
2023-08-03 12:43:34 -0600

    Update

--- devs_and_dragons.client/src/components/Navbar.vue
+++ devs_and_dragons.client/src/components/Navbar.vue
@@ -24,8 +24,8 @@
           </router-link>
         </li>
         <li>
-          <router-link :to="{ name: 'Campaign' }" class="btn lighten-30 selectable text-uppercase">
-            Campaign
+          <router-link :to="{ name: 'Campaign' }" class="btn lighten-30 selectable text-uppercase nav-shadow">
+            Campaigns
           </router-link>
         </li>
 
