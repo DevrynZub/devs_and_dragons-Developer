78e35f8a122fdd0cc3ee06393651b5dd1c44a90e
Andrew Allison <andrewallison5211@gmail.com>
2023-08-04 12:03:32 -0600

    Home page content

--- devs_and_dragons.client/src/pages/HomePage.vue
+++ devs_and_dragons.client/src/pages/HomePage.vue
@@ -14,9 +14,9 @@
       <div class="row justify-content-end pt-5 pe-5">
         <div class="col-md-6 col-12 home-card text-shadow p-4 mt-5 rounded elevation-3">
           <h4 class="text-center">
-            Foucs on The Adventure</h4>
+            Foucs On The Adventure</h4>
             <p>
-              Devs and Dragons' campaign management system gives you control over your campaigns. Search from a list of campaigns created by other DM's, or add your own. Important details like the party size and session dates are available to help in finding the experience best suited for you, so players and Dm's alike can spend less time seeking quests and more time exploring their worlds.
+              Devs and Dragons' campaign management system gives you control over your campaigns. Search from a list of campaigns created by other DM's, or add your own. Important details like the party size and session dates are available to help in finding the experience best suited for you, so players and DM's alike can spend less time seeking quests and more time exploring their worlds.
             </p>
         </div>
       </div>
@@ -25,9 +25,13 @@
         <div class="col-md-6 col-12 home-card text-shadow p-4 mt-5 rounded elevation-3">
           <h4 class="text-center">Share The Knowledge</h4>
           <p>
-              In a game where keeping track of information, entities, and abiltites can be the difference between victory and defeat, note taking becomes a critical task of the party. The team at Devs and Dragons knows this, and provides each player within a campaign a noke space. Quickly jot down important locations or the name of a prominent character that can be easily referenced at any time by all the party members. Coordinate and share information across the party like never before.
+              In a game where keeping track of information, entities, and abiltites can be the difference between victory and defeat, note taking becomes a critical task of the party. The team at Devs and Dragons knows this, and provides each player within a campaign a note space. Quickly jot down important locations or the name of a prominent character that can be easily referenced at any time by all the party members. Coordinate and share information across the party like never before.
           </p>
         </div>
+      </div>
+
+      <div class="row justify-content-end pt-5 pe-5">
+        <div class="col-md-6 col-12 home-card text-shadow p-4 mt-5 rounded elevation-3"></div>
       </div>
 
     </div>
