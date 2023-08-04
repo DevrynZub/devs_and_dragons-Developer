3de5b835b9c756a3ce1282a1c0fe27951a844efb
Jeremy <ayamaikuko@gmail.com>
2023-08-04 10:12:42 -0600

    entity model

---
+++ devs_and_dragons.client/src/models/Entity.js
@@ -0,0 +1,17 @@
+
+
+export class Entity {
+  constructor(data) {
+    this.id = data.id
+    this.type = data.type
+    this.desc = data.desc
+    this.connections = data.connections
+    this.body = data.body
+    this.name = data.name
+    this.imgUrl = data.imgUrl
+    this.creatorId = data.creatorId
+    this.isPrivate = data.isPrivate
+    this.tags = data.tags
+    this.Creator = data.Creator
+  }
+}
