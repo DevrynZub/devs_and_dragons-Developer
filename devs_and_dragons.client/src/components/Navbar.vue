792292f141a829395232ef07554231ffb3f076cd
Andrew Allison <andrewallison5211@gmail.com>
2023-08-04 14:52:56 -0600

    Home page mobile formatted

--- devs_and_dragons.client/src/components/Navbar.vue
+++ devs_and_dragons.client/src/components/Navbar.vue
@@ -52,7 +52,9 @@ export default {
   background-image: linear-gradient(rgba(69, 61, 61, 0.5), rgb(144, 6, 6));
   box-shadow:
     inset 0 0 20px 7px #b44612,
-    10px 3px 100px 5px #83d1cd;
+    inset 0 2px 10px 2px #ecd718,
+    4px 0 10px 2px #ffdc41,
+    0 3px 100px 5px #83d1cd;
 }
 
 .text-shadow {
