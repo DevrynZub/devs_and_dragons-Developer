911f0713ea070f210bca14a0b9115e5a623e0906
Riley <riley@idahosuperiorcustoms.com>
2023-08-07 14:21:31 -0600

    About Pg Updates

--- devs_and_dragons.client/src/pages/AboutPage.vue
+++ devs_and_dragons.client/src/pages/AboutPage.vue
@@ -61,8 +61,8 @@
         <div class="col-12 col-md-5 card m-2 p-2 align-items-center">
           <div class="mt-4">Riley Thomas</div>
           <div>
-            <a class="mdi mdi-github mx-2 fs-2" href=""></a>
-            <a class="mdi mdi-linkedin fs-2" href=""></a>
+            <a class="mdi mdi-github mx-2 fs-2" href="https://github.com/Rilezzz"></a>
+            <a class="mdi mdi-linkedin fs-2" href="https://www.linkedin.com/in/idahosuperiorcustoms/"></a>
           </div>
         </div>
         <div class="col-12 col-md-5 card m-2 p-2 align-items-center">
