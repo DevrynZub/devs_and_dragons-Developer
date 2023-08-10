079753c4c8a8df8c7ee787366abf6aadad3558d9
Riley <riley@idahosuperiorcustoms.com>
2023-08-10 10:58:32 -0600

    Updates

---
+++ devs_and_dragons.client/src/components/SpellsComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-11 col-md-9">
+                <div>
+                    <h3> {{ spells?.name }}</h3>
+                </div>
+                <div>
+                    <p>{{ }}</p>
+                </div>
+
+
+
+
+
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import { computed } from 'vue';
+import { AppState } from '../AppState.js';
+
+export default {
+    setup() {
+        return {
+            spells: computed(() => AppState.selectedResult)
+
+
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
