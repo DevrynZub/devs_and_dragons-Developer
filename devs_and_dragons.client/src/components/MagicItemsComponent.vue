8eb60691ad44ef9c1e24f7df3fffa1e5471a533b
Brandon Riggs <aceriggs7@gmail.com>
2023-08-10 11:51:22 -0600

    magic-items component

---
+++ devs_and_dragons.client/src/components/MagicItemsComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div>
+        Name: {{ magicItems?.name }}
+      </div>
+      <div>
+        Category: {{ magicItems?.equipment_category.name }}
+      </div>
+      <div>
+        Rarity: {{ magicItems?.rarity }}
+      </div>
+      <div>
+        Description: {{ magicItems?.desc }}
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { computed } from "vue";
+import { AppState } from "../AppState.js";
+
+export default {
+  setup() {
+    return {
+      magicItems: computed(() => AppState.selectedResult)
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
