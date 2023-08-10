fc8a20d137dc968cb100807969747dd8bc952e75
Riley <riley@idahosuperiorcustoms.com>
2023-08-10 11:02:45 -0600

    Updates to Spells

--- devs_and_dragons.client/src/components/SpellsComponent.vue
+++ devs_and_dragons.client/src/components/SpellsComponent.vue
@@ -6,13 +6,16 @@
                     <h3> {{ spells?.name }}</h3>
                 </div>
                 <div>
-                    <p>{{ }}</p>
+                    <p>{{ spells.range }}</p>
+                    <p>{{ spells.components }}</p>
+                    <p>{{ spells.material }}</p>
+                    <p>{{ spells.ritual }}</p>
+                    <p>{{ spells.duration }}</p>
+                    <p>{{ spells.concentration }}</p>
+                    <p>{{ spells.casting_time }}</p>
+                    <p>{{ spells.classes }}</p>
+                    <p>{{ spells.desc }}</p>
                 </div>
-
-
-
-
-
             </div>
         </div>
     </div>
