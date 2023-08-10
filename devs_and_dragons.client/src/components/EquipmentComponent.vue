c989816dd14dfde93133e3045c67b56abe5b6e0e
Jeremy <ayamaikuko@gmail.com>
2023-08-10 16:59:22 -0600

    equipment component adjustment

--- devs_and_dragons.client/src/components/EquipmentComponent.vue
+++ devs_and_dragons.client/src/components/EquipmentComponent.vue
@@ -8,19 +8,40 @@
         Damage Die: {{ equipment.damage.damage_dice }}
       </div>
       <div v-if="equipment?.range">
-        Range: {{ equipment.range.normal }}/{{ equipment.range?.long }} Feet
+        Range: {{ equipment.range.normal }}<span v-if="equipment.weapon_range == 'Ranged'">/{{ equipment.range?.long
+        }}</span> Feet
       </div>
-      <div v-if="equipment?.properties">
-        <!-- <div v-for="e in equipment."></div> -->
+      <div v-if="equipment?.vehicle_category">
+        {{ equipment.vehicle_category }}
+      </div>
+      <div v-if="equipment?.speed">
+        Speed: {{ equipment.speed.quantity }} {{ equipment.speed.unit }}
+      </div>
+      <div v-if="equipment?.properties.length > 0">
+        {{ equipment?.name }} properties: <span v-for="e in equipment.properties" :key="e.name"> {{ e.name }}, </span>
+      </div>
+      <div v-if="equipment?.armor_category">
+        {{ equipment.armor_category }} <span v-if="equipment.armor_category != 'Shield'">Armor</span>
+      </div>
+      <div v-if="equipment?.armor_class">
+        AC: {{ equipment.armor_class.base }},
+        Dex Bonus: {{ equipment.armor_class.dex_bonus }},
+        <span v-if="equipment.armor_class?.max_bonus">Max Bonus: {{ equipment.armor_class.max_bonus }}</span>
+      </div>
+      <div v-if="equipment?.stealth_disadvantage">
+        Stealth Disadvantage: {{ equipment.stealth_disadvantage }}
+      </div>
+      <div v-if="equipment?.category_range">
+        {{ equipment.category_range }}
       </div>
       <div>
         Cost: {{ equipment?.cost.quantity }}{{ equipment?.cost.unit }}
       </div>
-      <div>
-        Item Weight: {{ equipment?.weight }}
+      <div v-if="equipment?.weight">
+        Item Weight: {{ equipment?.weight }} <span v-if="equipment.weight <= 1">lb.</span> <span v-else>lbs.</span>
       </div>
-      <div>
-        Description: {{ equipment?.desc }}
+      <div v-if="equipment?.desc.length > 0">
+        Description: <span v-for="(e, index) in equipment?.desc" :key="index">{{ e }}</span>
       </div>
     </div>
   </div>
