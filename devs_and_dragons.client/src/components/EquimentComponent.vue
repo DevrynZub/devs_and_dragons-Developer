336e2293120b85b191d5df9f6b1ad5e332baa793
Brandon Riggs <aceriggs7@gmail.com>
2023-08-10 10:49:25 -0600

    equipment component

---
+++ devs_and_dragons.client/src/components/EquimentComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  {{ equipment.name }}
+</template>
+
+
+<script>
+import { computed } from "vue";
+import { AppState } from "../AppState.js";
+
+export default {
+  setup() {
+    return {
+      equipment: computed(() => AppState.selectedResult)
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
